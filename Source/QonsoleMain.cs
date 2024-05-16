d43fce96aec2c4c9df189c88898cf860428494c2
U-ZAMMY-GAMING\stoiko <stoiko.todorov@gmail.com>
2024-05-16 17:08:51 +0300

    Show dynamic draws classes.

--- Source/QonsoleMain.cs
+++ Source/QonsoleMain.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Reflection;
+using System.Collections.Generic;
 using UnityEngine;
 using Verse;
 
@@ -10,7 +11,10 @@ namespace Qon {
 public static class QonsoleMain {
 
     // this is a Qonsole variable
+    [Description( "Demo a dynamic QGL message." )]
     static bool ShowQglMessage_kvar = false;
+    [Description( "Show dynamic draws classes." )]
+    static bool ShowDynamicDraws_kvar = false;
 
     static QonsoleMain() {
         try {
@@ -23,11 +27,14 @@ public static class QonsoleMain {
     }
 
     static void QonsolePreConfig_kmd( string [] _ ) {
+        // changing this will reset the console variables to defaults
         Cellophane.ConfigVersion_kvar = 10;
     }
     
     static void QonsoleTick_kmd( string [] _ ) {
         try {
+
+        ShowDynamicDraws();
 
         if ( ShowQglMessage_kvar ) {
             int n = ( ( int )( Time.unscaledTime * 1000 ) >> 9 ) % 4;
@@ -49,6 +56,34 @@ public static class QonsoleMain {
         MonoBehaviour [] behs = GameObject.FindObjectsOfType<MonoBehaviour>();
         foreach ( var b in behs ) {
             Qonsole.Log( $"{b.name} {b.GetType()}" );
+        }
+    }
+
+    static void ShowDynamicDraws() {
+        if ( ! ShowDynamicDraws_kvar ) {
+            return;
+        }
+
+        DynamicDrawManager ddm = Find.CurrentMap?.dynamicDrawManager;
+        if ( ddm == null ) {
+            return;
+        }
+
+        FieldInfo drawThingsInfo = ddm.GetType().GetField( "drawThings",
+                                            BindingFlags.NonPublic | BindingFlags.Instance );
+        if ( drawThingsInfo == null ) {
+            return;
+        }
+
+        var drawThings = drawThingsInfo.GetValue( ddm ) as List<Thing>;
+        if ( drawThings == null ) {
+            return;
+        }
+
+        var dta = drawThings.ToArray();
+        foreach ( var dt in dta ) {
+            Vector3 pos = dt.DrawPos;
+            QGL.LatePrintWorld( dt.GetType().ToString(), pos, color: Color.magenta );
         }
     }
 }
